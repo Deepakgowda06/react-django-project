99d431b8901a0d0f7db7e28dab7b57076beabcb1
Deepak Gowda k G <deepakgowda747@gmail.com>
2026-02-04 23:04:59 +0530

    Update settings.py

--- Backend/travels/travels/settings.py
+++ Backend/travels/travels/settings.py
@@ -23,8 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-x*#c#sue1j8^d#7lph-j1o+te)pj8!o+cm2z$1(we7sf(e!dlh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
+
 
 
 
