8c2e2733a8f569cb16ceaa553d9f48bb0488365d
Deepak Gowda k G <deepakgowda747@gmail.com>
2026-02-04 23:13:37 +0530

    Update views.py

--- Backend/travels/bookings/views.py
+++ Backend/travels/bookings/views.py
@@ -11,6 +11,8 @@ from rest_framework import status,generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
+
 
 def home(request):
     return HttpResponse("Welcome to your Django backend!")
