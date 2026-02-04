926c15b375bb6f190f0dcc011fb188e663251a86
Deepak Gowda k G <deepakgowda747@gmail.com>
2026-02-04 22:33:11 +0530

    Set DEBUG to False in settings.py
    Changed DEBUG setting to False for production.

--- Backend/travels/travels/settings.py
+++ Backend/travels/travels/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-x*#c#sue1j8^d#7lph-j1o+te)pj8!o+cm2z$1(we7sf(e!dlh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ['react-django-project-6.onrender.com', '127.0.0.1', 'localhost']
 
 
